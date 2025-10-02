0650b568dc694741cff8df32f5507a5875b7375c
Neesh <neeshellis@gmail.com>
2025-10-02 17:44:15 +1300

    merge

--- Backend/controllers/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-userController: require('./userController'),
-accommodationController: require('./accommodationController'),
-bookingController: require('./bookingController'),
-reviewController: require('./reviewController'),
-activitiesController: require('./activitiesController'),
-contactController: require('./contactController'),
-initialController: require('./initialController')
-}
